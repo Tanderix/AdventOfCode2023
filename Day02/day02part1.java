0d9fac0bb5a0946d5a202670eddc82647ffae7c4
Andrea Carozzi <carozzi.andrea@gmail.com>
2023-12-02 10:35:46 +0100

    Day02 Part 1 solution

---
+++ Day02/day02part1.java
@@ -0,0 +1,52 @@
+package Day02;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Map;
+
+public class day02part1 {
+    public static void main(String[] args) {
+        String filePath = "./Day02/day02.txt";
+        Map<String, Integer> bag = Map.of(
+            "red", 12,
+            "green", 13,
+            "blue", 14
+        );
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+                String line;
+                String[] sets;
+                String[] games;
+                String[] info;
+                int id = 1;
+                int IDsum = 0;
+                boolean possible = true;
+
+                while ((line = br.readLine()) != null) {
+                    sets = line.split(":")[1].trim().split(";");
+                    for (String set : sets) {
+                        games = set.trim().split(",");
+                        for (String game : games) {
+                            info = game.trim().split(" ");
+                            if(bag.get(info[1]) < Integer.parseInt(info[0])){
+                                possible = false;
+                            }
+                        }
+                    }
+                    System.out.println("Game " + id + " is " + possible);
+                    if(possible){
+                        IDsum+=id;
+                    }else{
+                        possible = true;
+                    }
+                    id++;
+                }
+
+                System.out.println("Part 1: " + IDsum);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+}
